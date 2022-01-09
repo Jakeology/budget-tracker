4892f36be6f018ba498de5f7b553d61ecf0bd025
Jake <xxjakexx14@gmail.com>
2022-01-08 21:48:23 -0500

    removed console logs to reduce spam

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,6 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -24,7 +23,6 @@ self.addEventListener("activate", function (e) {
       return Promise.all(
         keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
-            console.log("deleting cache : " + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
@@ -34,21 +32,13 @@ self.addEventListener("activate", function (e) {
 });
 
 self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
         return request;
       } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
         return fetch(e.request);
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   );
 });
